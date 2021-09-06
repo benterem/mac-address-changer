8c82ed9381910e2583e6230865a2560b4d66e609
benterem <benterem@gmail.com>
2021-09-06 18:44:05 +0300

    remove hardcoded elements

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,10 @@
 
 
 import subprocess
+import sys
+
+interface = sys.argv[0]
+
 
 subprocess.call("ifconfig wlan0 down", shell=True)
 subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
