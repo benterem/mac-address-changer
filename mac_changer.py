04c87f44110ab17e56a2d710a3ab3aad4027d2be
benterem <benterem@gmail.com>
2021-09-07 12:42:41 +0300

    fString in commands

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,7 @@ new_mac = sys.argv[2]
 
 print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
-#subprocess.call("ifconfig wlan0 down", shell=True)
-#subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-#subprocess.call("ifconfig wlan0 up", shell=True)
+#subprocess.call(f"ifconfig {interface} down", shell=True)
+#subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
+#subprocess.call(f"ifconfig {interface} up", shell=True)
 
