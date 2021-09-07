9285d6e69480f4e2a94414c9791fb142981b55f0
benterem <benterem@gmail.com>
2021-09-07 16:59:56 +0300

    more secure system calls

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,7 @@ new_mac = sys.argv[2]
 
 print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
-#subprocess.call(f"ifconfig {interface} down", shell=True)
-#subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
-#subprocess.call(f"ifconfig {interface} up", shell=True)
-
+#more secure, as it treats all elements in list as one command
+#subprocess.call(["ifconfig", interface, "down"])
+#subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+#subprocess.call(["ifconfig", interface, "up"])
