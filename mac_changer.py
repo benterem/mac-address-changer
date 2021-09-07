3b12265f257cf75c31b2c008d0191321894804b0
benterem <benterem@gmail.com>
2021-09-07 12:20:42 +0300

    fString

--- mac_changer.py
+++ mac_changer.py
@@ -5,9 +5,11 @@ import subprocess
 import sys
 
 interface = sys.argv[1]
+new_mac = sys.argv[2]
 
+print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+#subprocess.call("ifconfig wlan0 down", shell=True)
+#subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
+#subprocess.call("ifconfig wlan0 up", shell=True)
 
